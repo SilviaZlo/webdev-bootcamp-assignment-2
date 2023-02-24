9d86e8d002fedea60538b46252c2fda5649a34e7
SilviaZlo <silvianevesmayer@gmail.com>
2023-02-23 21:43:09 -0500

    added js code for toggle button to open slide out nav bar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// getting the toggleButton element that is inside my code. This will return an array and I want the first element of the array (index zero), which is the toggle button
+// getting the toggleButton element inside my code; will return an array and I´m selecting the first element of the array (index zero), which is the toggle button
 const toggleButton = document.getElementsByClassName('toggleButton')[0]
 
 //getting the navBar ul
@@ -7,4 +7,4 @@ const navBarLinks = document.getElementsByClassName('navBarLinks')[0]
 toggleButton.addEventListener('click', () => {
     navBarLinks.classList.toggle('active')
 })
-//we´re goona get the toggle button, add an eventlistener that is going to be clicked, and then we´re gonna run a certain function. This function will take the navBarLInks, access all the different classes on it (.classList) and toggle the active class (toggle('active'). This means that if the active class doesn´t exist it will add it, and if the active class does exist it will remove it. 
+//getting the toggle button, adding an eventlistener that is going to be clicked, and then runing the function, which will take the navBarLInks, access all the different classes on it (.classList) and toggle the active class (toggle('active'). = if the active class doesn´t exist it will add it, and if the active class does exist it will remove it. 
